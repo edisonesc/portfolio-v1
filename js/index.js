14a51e0656e98ba7ac5c2613e10adaf652698d92
Edison Escabarte <edi.escx@gmail.com>
2020-11-21 03:51:10 +0800

    [refactor] add countdown

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 window.onload = function() {
     initTabs();
+    initCountdown();
 }
 
 function initTabs() {
@@ -26,4 +27,27 @@ function openTab(evt, tabName) {
 
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function initCountdown() {
+    setCountdown();
+    setInterval(setCountdown, 86400);
+}
+
+function setCountdown() {     
+    var today = new Date();
+    var targetDate = new Date("January 1, 2030");
+    var msPerDay = 24 * 60 * 60 * 1000;
+    var timeLeft = (targetDate.getTime() - today.getTime());
+    var e_daysLeft = timeLeft / msPerDay;
+    var daysLeft = Math.floor(e_daysLeft);
+    var yearsLeft = 0;
+    if (daysLeft > 365) {
+    yearsLeft = Math.floor(daysLeft / 365);
+    daysLeft = daysLeft % 365;
+    }
+    var e_hrsLeft = (e_daysLeft - daysLeft) * 24;
+    var hrsLeft = Math.floor(e_hrsLeft);
+    var minsLeft = Math.floor((e_hrsLeft - hrsLeft) * 60);    
+    document.getElementById('countdown').innerHTML = yearsLeft + "YR" + daysLeft + "D";
 }
